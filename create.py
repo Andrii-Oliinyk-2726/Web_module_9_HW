f4f8dcef1ca93dfb9234e21d57124b07337dd1e9
Andrii-Oliinyk-2726 <123332365+Andrii-Oliinyk-2726@users.noreply.github.com>
2023-06-04 23:02:05 +0300

    Create create.py

---
+++ create.py
@@ -0,0 +1,31 @@
+from models import Quote, Author, TextPost
+import json
+
+
+def load_json_quote_to_database(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        date = json.load(file)
+        for item in date:
+            author_name = item['author']
+            author = Author.objects(fullname=author_name).first()
+            print(author, type(author))
+            if not author:
+                author = Author(fullname=author_name)
+                author.save()
+
+            quote = Quote(author=author, quote=item['quote'], tags=item['tags'])
+            quote.save()
+
+
+def load_json_author_to_database(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        for item in data:
+            document = Author(**item)
+            document.save()
+
+
+if __name__ == '__main__':
+
+    load_json_author_to_database('authors.json')
+    load_json_quote_to_database('quotes.json')
